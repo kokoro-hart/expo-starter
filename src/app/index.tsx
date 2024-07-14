9bc1f0adeb65eebf07ba9719be2ac0f80f896651
kokoro-hart <tobita@wd-flat.com>
2024-07-14 21:40:05 +0900

    remove: Unnecessary files

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>index</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
